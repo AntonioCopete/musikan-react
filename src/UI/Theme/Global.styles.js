964f8d9e41b1057e99a4b85ff9aa34b3299aa605
AranBeitia <arantza.beitia@gmail.com>
2022-01-26 16:33:05 +0100

    Build: install libs

---
+++ src/UI/Theme/Global.styles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+	body {
+		background-color: blue;
+		transition: background-color 0.5s linear;
+	}
+`
