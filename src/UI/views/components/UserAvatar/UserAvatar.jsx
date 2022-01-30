dde7f16923018e72ee7af777c5f927c6141af7cd
CarlosVelilla <carlosvelillaf@hotmail.com>
2022-01-30 20:17:21 +0100

    Fix.: Show user own pic on Dashboard Avatar component

--- src/UI/views/components/UserAvatar/UserAvatar.jsx
+++ src/UI/views/components/UserAvatar/UserAvatar.jsx
@@ -1,12 +1,14 @@
 import { Avatar } from '@mui/material'
 import { AvatarContainer } from './UserAvatar.styles'
-import defaultPic from '../../../img/default-pic.jpg'
+
+import { useSelector } from "react-redux"
 
 function UserAvatar({ showUserMenu, setShowUserMenu }) {
+  const { profilePicture } = useSelector(state => state.auth.currentUser)
   return (
     <AvatarContainer>
       <Avatar
-        src={defaultPic}
+        src={profilePicture}
         alt="Firstname Lastname"
         sx={{ width: 56, height: 56 }}
         onClick={() => {
