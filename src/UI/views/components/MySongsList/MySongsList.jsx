3c8a95685f47646f15adfda8770a54b1bd0aaf04
AranBeitia <arantza.beitia@gmail.com>
2022-02-01 17:42:52 +0100

    Error

--- src/UI/views/components/MySongsList/MySongsList.jsx
+++ src/UI/views/components/MySongsList/MySongsList.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { getTracks } from '../../../../redux/track/actions'
-import { trackSelector } from '../../../../redux/track/selectors'
+// import { trackSelector } from '../../../../redux/track/selectors'
 
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
@@ -18,7 +18,7 @@ function MySongsList() {
 
   useEffect(() => {
     dispatch(getTracks())
-  }, [])
+  }, [dispatch])
 
   const [checked, setChecked] = useState([1])
 
@@ -37,35 +37,36 @@ function MySongsList() {
 
   return (
     <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      {track.map((value) => {
-        const labelId = `checkbox-list-secondary-label-${value}`
-        return (
-          <ListItem
-            key={value._id}
-            secondaryAction={
-              <Checkbox
-                edge="end"
-                onChange={handleToggle(value)}
-                checked={checked.indexOf(value) !== -1}
-                inputProps={{ 'aria-labelledby': labelId }}
-                sx={{ width: '50px' }}
-              />
-            }
-          >
-            <ListItemButton>
-              <ListItemAvatar>
-                <Avatar
-                  alt={`Avatar n°${value + 1}`}
-                  src={value.thumbnail}
-                  variant="square"
+      {track &&
+        track.map((value) => {
+          const labelId = `checkbox-list-secondary-label-${value}`
+          return (
+            <ListItem
+              key={value._id}
+              secondaryAction={
+                <Checkbox
+                  edge="end"
+                  onChange={handleToggle(value)}
+                  checked={checked.indexOf(value) !== -1}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                  sx={{ width: '50px' }}
                 />
-              </ListItemAvatar>
-              <ListItemText id={labelId} primary={`Song ${value.name}`} />
-              <p>genre -{value.genre} </p>
-            </ListItemButton>
-          </ListItem>
-        )
-      })}
+              }
+            >
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={`Avatar n°${value + 1}`}
+                    src={value.thumbnail}
+                    variant="square"
+                  />
+                </ListItemAvatar>
+                <ListItemText id={labelId} primary={`Song ${value.name}`} />
+                <p>genre -{value.genre} </p>
+              </ListItemButton>
+            </ListItem>
+          )
+        })}
     </List>
   )
 }
