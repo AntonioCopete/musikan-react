9f23df50281179bd1afd43a026a71fdfd0211012
AranBeitia <arantza.beitia@gmail.com>
2022-02-03 11:19:41 +0100

    Styles: input form

--- src/UI/styles/GlobalComponents/Input.js
+++ src/UI/styles/GlobalComponents/Input.js
@@ -3,19 +3,19 @@ import styled from 'styled-components'
 export const InputGroup = styled.div`
   display: grid;
   grid-template-columns: 15% 85%;
+  place-content: center;
   max-width: 23.75rem;
   width: 100%;
   height: 3.437rem;
   margin: 1rem 0;
-  padding: 0 0.4rem;
+  padding: 0 1rem;
   background-color: ${(props) => props.theme.colors.darkGrey};
   border-radius: 3.437rem;
 
-  & i {
-    text-align: center;
-    line-height: 3.437rem;
+  & svg {
     color: ${(props) => props.theme.colors.primary};
     font-size: 1.1rem;
+    justify-self: center;
   }
 
   & input {
@@ -26,7 +26,6 @@ export const InputGroup = styled.div`
     border: none;
     outline: none;
     background: none;
-    border-radius: inherit;
   }
 
   & input::placeholder {
