7423965ddd46e841ad0acc37fba1d3834b2ab60c
AranBeitia <arantza.beitia@gmail.com>
2022-02-09 13:50:57 +0100

    Feat: add playlist user follows in dashboard

--- src/UI/views/pages/Dashboard/Dashboard.jsx
+++ src/UI/views/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,44 @@
+import { useState, useLayoutEffect } from 'react'
+import { useSelector } from 'react-redux'
+import api from '../../../../api'
 import HomeHeader from './HomeHeader/HomeHeader'
 import { Header, Main } from '../../layout/Layout.styles'
 
+import Playlist from '../MyPlaylists/Playlist/Playlist'
+
 function Dashboard() {
+  const { _id } = useSelector((state) => state.auth.currentUser)
+  const [playlists, setPlaylists] = useState({})
+
+  useLayoutEffect(() => {
+    getUserPlaylists()
+  }, [])
+
+  const getUserPlaylists = async () => {
+    const response = await api.getUserPlaylists(_id)
+    setPlaylists(response.data.data)
+  }
+
   return (
     <>
       <Header>
         <HomeHeader />
       </Header>
       <Main>
-        <h1>home content</h1>
+        {/* <h2>Popular profiles</h2>
+        <h2>Top playlists</h2>
+
+        <h2>Popular artists</h2>*/}
+        <h2>Playlists you follow</h2>
+        {playlists?.followed?.length > 0 ? (
+          <Playlist
+            list={playlists.followed}
+            followed={true}
+            reload={getUserPlaylists}
+          />
+        ) : (
+          <p>You dont follow any playlist yet</p>
+        )}
       </Main>
     </>
   )
