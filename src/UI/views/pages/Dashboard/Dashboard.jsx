1b601164476cd74b03c4856717e9447d46d9ae6f
AranBeitia <arantza.beitia@gmail.com>
2022-02-13 18:20:52 +0100

    Styles: home

--- src/UI/views/pages/Dashboard/Dashboard.jsx
+++ src/UI/views/pages/Dashboard/Dashboard.jsx
@@ -5,7 +5,9 @@ import HomeHeader from './HomeHeader/HomeHeader'
 import { Header, Main } from '../../layout/Layout.styles'
 
 import Playlist from '../MyPlaylists/Playlist/Playlist'
-import PopularUsers from './PopularUsers/PopularUsers'
+import PopularUsers from './HomeContent/PopularUsers/PopularUsers'
+import TopPlaylists from './HomeContent/TopPlaylists/TopPlaylists'
+import HomeContent from './HomeContent/HomeContent'
 
 function Dashboard() {
   const { _id } = useSelector((state) => state.auth.currentUser)
@@ -26,21 +28,23 @@ function Dashboard() {
         <HomeHeader />
       </Header>
       <Main>
-        <h2>Popular profiles</h2>
-        <PopularUsers />
-        {/*   <h2>Top playlists</h2>
-
-        <h2>Popular artists</h2>*/}
-        <h2>Playlists you follow</h2>
-        {playlists?.followed?.length > 0 ? (
-          <Playlist
-            list={playlists.followed}
-            followed={true}
-            reload={getUserPlaylists}
-          />
-        ) : (
-          <p>You dont follow any playlist yet</p>
-        )}
+        <HomeContent title={'Popular profiles'} content={<PopularUsers />} />
+        <HomeContent title={'Top playlists'} content={<TopPlaylists />} />
+        <HomeContent title={'Popular artists'} content={<PopularUsers />} />
+        <HomeContent
+          title={'Playlists you follow'}
+          content={
+            playlists?.followed?.length > 0 ? (
+              <Playlist
+                list={playlists.followed}
+                followed={true}
+                reload={getUserPlaylists}
+              />
+            ) : (
+              <p>You dont follow any playlist yet</p>
+            )
+          }
+        />
       </Main>
     </>
   )
---
+++ src/UI/views/pages/Dashboard/HomeContent/TopPlaylists/TopPlaylists.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 35rem;
+  margin: 0 1rem;
+  border-radius: 10px;
+`
+
+export const ItemBlue = styled(ItemWrapper)`
+  background: #3936d0;
+`
+export const ItemGreen = styled(ItemWrapper)`
+  background: #53b724;
+`
+export const ItemMagenta = styled(ItemWrapper)`
+  background: #eb2a9a;
+`
+
+export const ItemContent = styled.div`
+  width: 15rem;
+  height: 10rem;
+  border-radius: 10px 0 0 10px;
+  background: ${(props) => `url(${props.bgImage})`};
+  background-size: cover;
+  background-position: center;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 13rem;
+    height: 13rem;
+  }
+`
+
+export const Text = styled.p`
+  margin: 1rem;
+  color: ${({ theme }) => theme.colors.text};
+`
